827e19fa796b155785b79ca30079537a016b8e59
Mouayed Keziz <m_keziz@estin.dz>
2024-05-06 15:49:47 +0100

    -

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,6 +1,9 @@
 "use client"
 import PostsSection from "@/components/home/posts-section"
+import { Button, buttonVariants } from "@/components/ui/button"
 import { api } from "@/trpc/react"
+import { Plus } from "lucide-react"
+import Link from "next/link"
 
 
 export default function Home() {
@@ -10,6 +13,12 @@ export default function Home() {
         <h1>all posts:</h1>
       </div>
       <PostsSection />
+      <div className="absolute bottom-0 right-0 m-3">
+        <Link href="/create-post" className={buttonVariants({ className: "gap-2" })}>
+          new post
+          <Plus className="w-5 h-5" />
+        </Link>
+      </div>
     </>
   )
 }
